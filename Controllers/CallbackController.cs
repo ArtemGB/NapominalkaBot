eb6687c9c9fdd2b876cb9ae7c970e741dc82bb02
ArtemGB <gmadrich@gmail.com>
2019-09-22 22:11:40 +0300

    Правка UserId в CallBackController.

--- Controllers/CallbackController.cs
+++ Controllers/CallbackController.cs
@@ -51,7 +51,7 @@ namespace VkBot.Controllers
                         var msg = Message.FromJson(new VkResponse(updates.Object));
                         if (!allUsers.Users.ContainsKey(msg.UserId.Value)) //Добавление нового пользователя.
                         {
-                            allUsers.Users.Add(msg.Id.Value, new VkUser(msg.UserId.Value));
+                            allUsers.Users.Add(msg.UserId.Value, new VkUser(msg.UserId.Value));
                             VKSendMsg(msg.PeerId.Value, MsgTexts.HelloNewUser);
                         }
                         else
